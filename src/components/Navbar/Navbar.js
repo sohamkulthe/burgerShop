143583bb5b3e9c84407ce98ee604a08e25cfef00
sohamkulthe <sohampkulthe@gmail.com>
2025-03-17 03:22:02 +0530

    font weight class created and added to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,17 +5,17 @@ import { AppBar, Box, Button, ToggleButton, ToggleButtonGroup, Toolbar, Typograp
 
 function Navbar() {
   return (
-    <AppBar position='absolute' sx={{top:'9%', left:'34%', width:'60%'}} className='navbar'>
+    <AppBar position='absolute' sx={{top:'9%', left:'34%', width:'60%'}} className='display-flex navbar'>
         <Toolbar sx={{display:'flex', justifyContent:'space-between'}}>
-            <Box className='navbar-links'>
-            <Typography className='navbar-texts' color='black' style={{textDecoration: 'none'}} component={Link} to="/">Home</Typography>
-            <Typography className='navbar-texts' color='black' component={Link} to='/burgers'>Burgers</Typography>
-            <Typography className='navbar-texts' color='black' component={Link} to="/offers">Offers</Typography>
-            <Typography className='navbar-texts' color='black' component={Link} to="/shop">Shop</Typography>
+            <Box className='display-flex navbar-links'>
+            <Typography className='font-weight-700 navbar-texts' color='black' style={{textDecoration: 'none'}} component={Link} to="/">Home</Typography>
+            <Typography className='font-weight-700 navbar-texts' color='black' component={Link} to='/burgers'>Burgers</Typography>
+            <Typography className='font-weight-700 navbar-texts' color='black' component={Link} to="/offers">Offers</Typography>
+            <Typography className='font-weight-700 navbar-texts' color='black' component={Link} to="/shop">Shop</Typography>
             </Box>
             <Box className="">
-                <Button className='bg-light-grey br-left-50 register-btn'  component={Link} to="/register">Register</Button>
-                <Button className='bg-light-red br-right-50 login-btn' component={Link} to="/login">Login</Button>
+                <Button className='bg-light-grey br-left-50 font-weight-700 register-btn'  component={Link} to="/register">Register</Button>
+                <Button className='bg-light-red br-right-50 font-weight-700 login-btn' component={Link} to="/login">Login</Button>
             </Box>
         </Toolbar>
     </AppBar>
